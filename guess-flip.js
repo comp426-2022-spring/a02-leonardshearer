06a1823482cbab56d05c7a83f8b21d74e732e3ea
Leonard Shearer <leojshe@unc.edu>
2022-03-31 19:05:44 -0400

    Added usage

--- guess-flip.js
+++ guess-flip.js
@@ -4,8 +4,9 @@ import minimist from "minimist"
 const args = minimist(process.argv.slice(2))
 
 const call = args['call'] || "invalid"
-if (call === "invalid") {
+if (!(call === "heads" || call === "tails")) {
     console.log("Error: no input")
+    console.log("Usage: node guess-flip --call=[heads|tails]")
 } else {
     console.log(coinFlipper.flipACoin(call))
 }
